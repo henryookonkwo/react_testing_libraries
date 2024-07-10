5a00a10d31d34be918a28d9dd5da502f44cbba38
Henry Okonkwo <47309196+henryookonkwo@users.noreply.github.com>
2024-07-10 13:06:03 -0400

    Testing table contents

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -25,4 +25,23 @@ test("render one row per user", () => {
   //Assertion: correct number of rows in the table
   expect(rows).toHaveLength(2);
 });
-test("render the email and name of each user", () => {});
+test("render the email and name of each user", () => {
+  //render the component
+  const users = [
+    {
+      name: "jane",
+      email: "jane@jane.com",
+    },
+    { name: "sam", email: "sam@sam.com" },
+  ];
+  render(<UserList users={users} />);
+
+  //   screen.logTestingPlaygroundURL();
+  for (let user of users) {
+    const name = screen.getByRole("cell", { name: user.name });
+    const email = screen.getByRole("cell", { name: user.email });
+
+    expect(name).toBeInTheDocument();
+    expect(email).toBeInTheDocument();
+  }
+});
